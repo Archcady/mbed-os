68749c51ea8a4fbd0bcebbf44f427a768275df99
Yuguo Zou <zouyuguo@realtek-sg.com>
2017-03-10 17:04:54 +0800

    Add Arduino pin definition

--- targets/TARGET_Realtek/TARGET_AMEBA/TARGET_RTL8195A/PinNames.h
+++ targets/TARGET_Realtek/TARGET_AMEBA/TARGET_RTL8195A/PinNames.h
@@ -164,6 +164,30 @@ typedef enum {
     DA_0  = (PORT_U<<4|0),
     DA_1  = (PORT_U<<4|1),
 
+      // Arduino connector namings
+
+    D0          = PA_6,
+    D1          = PA_7,
+    D2          = PA_5,
+    D3          = PD_4,
+    D4          = PD_5,
+    D5          = PA_4,
+    D6          = PA_3,
+    D7          = PA_2,
+    D8          = PB_4,
+    D9          = PB_5,
+    D10         = PC_0,
+    D11         = PC_2,
+    D12         = PC_3,
+    D13         = PC_1,
+    D14         = PB_3,
+    D15         = PB_2,
+    D16         = PA_1,
+    D17         = PA_0,
+    D18         = PE_1,
+    D19         = PE_5,
+
+
     // Generic signals namings
     LED1        = PB_4,
     LED2        = PB_5,
